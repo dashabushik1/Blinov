2c2f242458fdb886357820317eb41a51bbf9af78
Darya Bushik <darya.bushik@gmail.com>
2022-06-14 23:13:41 +0300

    change code

--- src/main/java/chapter1/task2/MaxMinNumbers.java
+++ src/main/java/chapter1/task2/MaxMinNumbers.java
@@ -3,16 +3,29 @@
  */
 package chapter1.task2;
 
-import java.util.Arrays;
-
 public class MaxMinNumbers {
 
     public static void main(String[] args) {
 
-        Arrays.sort(args);
-        int maxNumber = Integer.parseInt(args[args.length - 1]);
+        int[] masSorted = new int[args.length];
+        for (int i = 0; i < args.length; i++) {
+            masSorted[i] = Integer.parseInt(args[i]);
+        }
 
+        int value = 0;
+        for (int i = 0; i < masSorted.length; i++) {
+            for (int j = i + 1; j < masSorted.length; j++) {
+                if (masSorted[i] > masSorted[j]) {
+                    value = masSorted[j];
+                    masSorted[j] = masSorted[i];
+                    masSorted[i] = value;
+                }
+            }
+        }
+
+        System.out.println("Min number is: " + masSorted[0]);
+
+        int maxNumber = masSorted[masSorted.length - 1];
         System.out.println("Max number is: " + maxNumber);
-        System.out.println("Min number is: " + args[0]);
     }
 }

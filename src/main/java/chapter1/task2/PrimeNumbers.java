51870d510417580d771ce517cdd9c342a54f7f83
Darya Bushik <darya.bushik@gmail.com>
2022-06-14 21:05:00 +0300

    change code and fix formatting

--- src/main/java/chapter1/task2/PrimeNumbers.java
+++ src/main/java/chapter1/task2/PrimeNumbers.java
@@ -1,38 +1,24 @@
-/* 5. Ввести с консоли n целых чисел. На консоль вывести все простые числа. */
-
+/**
+ * 5. Ввести с консоли n целых чисел. На консоль вывести все простые числа.
+ */
 package chapter1.task2;
-
-
-import java.util.Scanner;
 
 public class PrimeNumbers {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter number of digits: ");
-        int size = scanner.nextInt();
-        System.out.println("Enter numbers: ");
-        int[] allNumbers = new int[size];
-
-        for (int i = 0; i < size; i++) {
-            allNumbers[i] = scanner.nextInt();
-        }
-
-        System.out.println("Prime numbers are: ");
-        for (int i = 0; i < allNumbers.length; i++) {
-            if (isPrime(allNumbers[i])) {
-                System.out.println(allNumbers[i]);
-
+        System.out.print("Prime numbers: ");
+        for (int i = 0; i < args.length; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j < Integer.parseInt(args[i]); j++) {
+                if (Integer.parseInt(args[i]) % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.print(args[i] + " ");
             }
         }
-    }
-    public static boolean isPrime(int n) {
-        for (int i = 2; i < n / 2; i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
     }
 }

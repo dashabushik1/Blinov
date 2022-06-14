a9a3ad0167acd7e1b2bcfe10f02aff97e68431f0
Darya Bushik <darya.bushik@gmail.com>
2022-06-14 23:50:48 +0300

    added code with do/while and while loops

--- src/main/java/chapter1/task1/OppositeOrder.java
+++ src/main/java/chapter1/task1/OppositeOrder.java
@@ -6,8 +6,25 @@ package chapter1.task1;
 public class OppositeOrder {
 
     public static void main(String[] args) {
+
         for (int i = args.length - 1; (i >= 0) && (i < args.length); i--) {
             System.out.println(args[i]);
         }
+
+
+//        int i = args.length - 1;
+//        do {
+//            System.out.println(args[i]);                   // loop do...while
+//            i--;
+//        } while ((i >= 0) && (i < args.length));
+
+//        int i = args.length - 1;
+//        while ((i >= 0) && (i < args.length)) {            // loop while
+//            System.out.println(args[i]);
+//            i--;
+//        }
+
     }
 }
+
+

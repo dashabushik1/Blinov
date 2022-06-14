9177d04c8e60728ef6662cea8be268f0a5d8f41c
Darya Bushik <darya.bushik@gmail.com>
2022-06-14 20:35:19 +0300

    change code and fix formatting

--- src/main/java/chapter1/task2/MaxMinNumbers.java
+++ src/main/java/chapter1/task2/MaxMinNumbers.java
@@ -1,31 +1,18 @@
-/* 2. Ввести с консоли n целых чисел. На консоль вывести наибольшее и наименьшее число.*/
-
+/**
+ * 2. Ввести с консоли n целых чисел. На консоль вывести наибольшее и наименьшее число.
+ */
 package chapter1.task2;
 
-import java.util.Scanner;
 import java.util.Arrays;
 
 public class MaxMinNumbers {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter number of digits: ");
-        int size = scanner.nextInt();
-        System.out.println("Enter numbers: ");
-        int[] allNumbers = new int[size];
+        Arrays.sort(args);
+        int maxNumber = Integer.parseInt(args[args.length - 1]);
 
-        for (int i = 0; i < size; i++) {
-            allNumbers[i] = scanner.nextInt();
-        }
-
-        Arrays.sort(allNumbers);
-
-        System.out.println("All entered numbers after sorting: " + Arrays.toString(allNumbers));
-
-        int max = allNumbers[allNumbers.length - 1];
-
-        System.out.println("большее число: " + max);
-        System.out.println("Min number: " + allNumbers[0]);
-
+        System.out.println("Max number is: " + maxNumber);
+        System.out.println("Min number is: " + args[0]);
     }
 }

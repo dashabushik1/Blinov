e205b70437f09e281a028338d3dcfbbf0447fd34
Darya Bushik <darya.bushik@gmail.com>
2022-06-16 14:24:49 +0300

    fix formatting

--- src/main/java/chapter1/task2/SortArrayByFrequency.java
+++ src/main/java/chapter1/task2/SortArrayByFrequency.java
@@ -1,33 +1,55 @@
-/* 8. Ввести с консоли n целых чисел. На консоль вывести числа в порядке убывания частоты встречаемости чисел. */
-
+/**
+ * 7. Ввести с консоли n целых чисел. На консоль вывести числа в порядке
+ * убывания частоты встречаемости чисел.
+ */
 package chapter1.task2;
-
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class SortArrayByFrequency {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter number of digits: ");
-        int size = scanner.nextInt();
-        System.out.println("Enter numbers: ");
-        int[] allInputNumbers = new int[size];
-
-        for (int i = 0; i < size; i++) {
-            allInputNumbers[i] = scanner.nextInt();
+        int[] inputNumbers = new int[args.length];
+        for (int i = 0; i < args.length; i++) {
+            inputNumbers[i] = Integer.parseInt(args[i]);
+            System.out.print(inputNumbers[i] + " ");
         }
 
-        int[] duplicates = new int[size];
-        for (int i = 0; i < allInputNumbers.length; i++) {
-            for (int j = 0; j < allInputNumbers.length; j++) {
-                if (allInputNumbers[i] == allInputNumbers[j]) {
+        System.out.println(" ");
+        int[] duplicates = new int[inputNumbers.length];
+        for (int i = 0; i < inputNumbers.length; i++) {
+            for (int j = 0; j < inputNumbers.length; j++) {
+                if (inputNumbers[i] == inputNumbers[j]) {
                     duplicates[i]++;
                 }
             }
+            System.out.print(duplicates[i] + " ");
         }
 
+        System.out.println(" ");
+        for (int i = 0; i < duplicates.length; i++) {
+            for (int j = i + 1; j < duplicates.length; j++) {
+                if (duplicates[i] < duplicates[j]) {
+                    int x = duplicates[j];
+                    duplicates[j] = duplicates[i];
+                    duplicates[i] = x;
+
+                    x = inputNumbers[j];
+                    inputNumbers[j] = inputNumbers[i];
+                    inputNumbers[i] = x;
+                } else if ((duplicates[i] == duplicates[j]) && (inputNumbers[i] < inputNumbers[j])) {
+                    int x = inputNumbers[j];
+                    inputNumbers[j] = inputNumbers[i];
+                    inputNumbers[i] = x;
+                }
+            }
+            System.out.print(inputNumbers[i] + " ");
+        }
+
+        System.out.println(" ");
+        for (int i = 0; i < args.length; i++) {
+            System.out.print(duplicates[i] + " ");
+        }
     }
 }
+
 

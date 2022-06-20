f09e436f4947c12ed5d2310f8b0ab6ac3813ac90
Darya Bushik <darya.bushik@gmail.com>
2022-06-20 18:47:18 +0300

    change code

--- src/main/java/chapter1/task2/DivByFive.java
+++ src/main/java/chapter1/task2/DivByFive.java
@@ -8,13 +8,16 @@ public class DivByFive {
     public static void main(String[] args) {
 
         String value = "";
-        for (int i = 0; i < args.length; i++) {
-            // как сделать проверку?
-            int x = Integer.parseInt(args[i]);
-            if ((x % 5 == 0) && (x % 7 == 0)) {
-                value += x + " ";
+        try {
+            for (int i = 0; i < args.length; i++) {
+                int x = Integer.parseInt(args[i]);
+                if ((x % 5 == 0) && (x % 7 == 0)) {
+                    value += x + " ";
+                }
             }
+            System.out.print("Numbers that are divisible by 5 and by 7: " + value);
+        } catch (NumberFormatException e) {
+            System.err.println("Entered is not a number.");
         }
-        System.out.print("Numbers that are divisible by 5 and by 7: " + value);
     }
 }

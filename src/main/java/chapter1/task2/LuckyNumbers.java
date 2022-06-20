9cd95fec4d90575980d7213577bbc10704f5a17b
Darya Bushik <darya.bushik@gmail.com>
2022-06-20 17:06:21 +0300

    change code

--- src/main/java/chapter1/task2/LuckyNumbers.java
+++ src/main/java/chapter1/task2/LuckyNumbers.java
@@ -11,18 +11,17 @@ public class LuckyNumbers {
         for (int i = 0; i < args.length; i++) {
             inputNumbers[i] = Integer.parseInt(args[i]);
 
-            int number = inputNumbers[i];
-            if ((inputNumbers[i] < 999999) && (inputNumbers[i] > 100000)) { // в промежутке от 100000 до 999999 (шестизначные)
+            if ((inputNumbers[i] < 1000000) && (inputNumbers[i] > 99999)) {
 
-                int dig1 = number % 10;
-                int dig2 = (number % 100) / 10;
-                int dig3 = (number % 1000) / 100;
-                int dig4 = (number % 10000) / 1000;
-                int dig5 = (number % 100000) / 10000;
-                int dig6 = (number % 1000000) / 100000;
+                int dig1 = inputNumbers[i] % 10;
+                int dig2 = (inputNumbers[i] % 100) / 10;
+                int dig3 = (inputNumbers[i] % 1000) / 100;
+                int dig4 = (inputNumbers[i] % 10000) / 1000;
+                int dig5 = (inputNumbers[i] % 100000) / 10000;
+                int dig6 = (inputNumbers[i] % 1000000) / 100000;
 
                 if ((dig1 + dig2 + dig3) == (dig4 + dig5 + dig6)) {
-                    System.out.println(inputNumbers[i] + " ");
+                    System.out.println("Lucky numbers: " + inputNumbers[i] + " ");
                 }
             }
         }

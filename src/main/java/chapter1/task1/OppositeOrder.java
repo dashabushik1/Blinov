5de2baa4cc48dd3f25a7c3c8bf59f43df4efbe38
Darya Bushik <darya.bushik@gmail.com>
2022-06-20 16:34:12 +0300

    change code

--- src/main/java/chapter1/task1/OppositeOrder.java
+++ src/main/java/chapter1/task1/OppositeOrder.java
@@ -7,23 +7,25 @@ public class OppositeOrder {
 
     public static void main(String[] args) {
 
+        if (args.length == 0) {
+            return;
+        }
+
         for (int i = args.length - 1; (i >= 0) && (i < args.length); i--) {
             System.out.println(args[i]);
         }
 
+        int i = args.length - 1;
+        do {
+            System.out.println(args[i]);                   // loop do...while
+            i--;
+        } while ((i >= 0) && (i < args.length));
 
-//        int i = args.length - 1;
-//        do {
-//            System.out.println(args[i]);                   // loop do...while
-//            i--;
-//        } while ((i >= 0) && (i < args.length));
-
-//        int i = args.length - 1;
-//        while ((i >= 0) && (i < args.length)) {            // loop while
-//            System.out.println(args[i]);
-//            i--;
-//        }
-
+        int j = args.length - 1;
+        while ((j >= 0) && (j < args.length)) {            // loop while
+            System.out.println(args[j]);
+            j--;
+        }
     }
 }
 

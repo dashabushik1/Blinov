6bbcd19c4dde808c9f313ebe4cf65eba9659b6c9
Darya Bushik <darya.bushik@gmail.com>
2022-06-21 14:42:08 +0300

    fix formatting

--- src/main/java/chapter1/task1/ComparePassword.java
+++ src/main/java/chapter1/task1/ComparePassword.java
@@ -8,13 +8,12 @@ public class ComparePassword {
     public static void main(String[] args) {
 
         if (args.length == 0) {
-            System.out.println("Password has not found");
+            System.out.println("Password not found.");
             return;
         }
 
         String stringSample = "11111";
         String stringInput = args[0];
-
 
         if (stringInput.equals(stringSample)) {
             System.out.println("Passwords match");

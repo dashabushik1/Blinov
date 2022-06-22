d0f9eafa3c32c7c30beb97fa11d63287d154dae8
Darya Bushik <darya.bushik@gmail.com>
2022-06-22 17:48:44 +0300

    change Date

--- src/main/java/chapter1/task1/DevInfo.java
+++ src/main/java/chapter1/task1/DevInfo.java
@@ -11,7 +11,11 @@ public class DevInfo {
     public static void main(String[] args) {
         String name = "Petrov";
         Date currentDate = new Date();  // создание текущей даты
-        Date endDate = new Date(122, 5, 22);
+        Date endDate = new Date();
+
+        endDate.setTime(currentDate.getTime() + 7 * 24 * 60 * 60 * 1000);
+
+
 
         System.out.println("Developer: " + name);
         System.out.println("Date and time of begin of the task: " + currentDate + ".");

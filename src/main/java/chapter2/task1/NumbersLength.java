0fb056495055319c40a992fb9ca17b3b0bc575f5
Darya Bushik <darya.bushik@gmail.com>
2022-07-06 15:44:23 +0300

    change code

--- src/main/java/chapter2/task1/NumbersLength.java
+++ src/main/java/chapter2/task1/NumbersLength.java
@@ -8,47 +8,23 @@ public class NumbersLength {
 
     public static void main(String[] args) {
 
-        int[] numberCounter = new int[args.length];
-        int[] inputNumbers = new int[args.length];
+        String[] arr = new String[args.length];
+
         for (int i = 0; i < args.length; i++) {
-            inputNumbers[i] = Integer.parseInt(args[i]);
-
-            int number = inputNumbers[i];
-            while (number > 0) {
-                numberCounter[i]++;           // считаем количество цифр в числах
-                number /= 10;
-            }
+            arr[i] = args[i];
         }
 
-        for (int i = 0; i < numberCounter.length; i++) {
-            for (int j = i + 1; j < numberCounter.length; j++) {
-                if(numberCounter[i] > numberCounter[j]) {
-                    int value = numberCounter[i];
-                    numberCounter[i] = numberCounter[j];
-                    numberCounter[j] = value;
-
-                    int x = inputNumbers[i];
-                    inputNumbers[i] = inputNumbers[j];
-                    inputNumbers[j] = x;
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j].length() > arr[j + 1].length()) {
+                    String value = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = value;
                 }
             }
-            System.out.print(inputNumbers[i] + " ");   // вывод по возрастанию значений длины
         }
-
-        System.out.println(" ");
-        for (int i = 0; i < numberCounter.length; i++) {
-            for (int j = i + 1; j < numberCounter.length; j++) {
-                if(numberCounter[i] < numberCounter[j]) {
-                    int value = numberCounter[j];
-                    numberCounter[j] = numberCounter[i];
-                    numberCounter[i] = value;
-
-                    int x = inputNumbers[j];
-                    inputNumbers[j] = inputNumbers[i];
-                    inputNumbers[i] = x;
-                }
-            }
-            System.out.print(inputNumbers[i] + " ");  // вывод по убыванию значений длины
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
         }
     }
 }

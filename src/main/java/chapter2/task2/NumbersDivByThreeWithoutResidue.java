bc8b966d4a85245f07b229ab1124384878f6bb76
Darya Bushik <darya.bushik@gmail.com>
2022-07-14 14:07:14 +0300

    add task

---
+++ src/main/java/chapter2/task2/NumbersDivByThreeWithoutResidue.java
@@ -0,0 +1,16 @@
+/**
+ * 4. Вывести на экран все числа от 1 до 100, которые делятся на 3 без остатка.
+ */
+package chapter2.task2;
+
+public class NumbersDivByThreeWithoutResidue {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 100; i++) {
+            if (i % 3 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}

46588ba74fa56e3d5971fe978f0168e0692ab05a
Darya Bushik <darya.bushik@gmail.com>
2022-07-14 19:42:42 +0300

    add task

---
+++ src/main/java/chapter2/task2/NumberSystem.java
@@ -0,0 +1,22 @@
+/**
+ * 7. Написать код программы, которая бы переводила числа из
+ * десятичной системы счисления в любую другую.
+ */
+package chapter2.task2;
+
+public class NumberSystem {
+
+    public static void main(String[] args) {
+
+        int[] arguments = new int[args.length];
+        for (int i = 0; i < args.length; i++) {
+            arguments[i] = Integer.parseInt(args[i]);
+        }
+
+        int numberToConvert = arguments[0];
+        int foundationOfNewNumberSystem = arguments[1];
+
+        System.out.println("Number " + numberToConvert + " in the chosen number system is equal to " +
+                Integer.toString(numberToConvert, foundationOfNewNumberSystem) + ".");
+    }
+}

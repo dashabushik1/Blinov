f71a4484ea33c74d8f50941f36533d4fe7290a1a
Darya Bushik <darya.bushik@gmail.com>
2022-07-14 21:01:12 +0300

    unfinished task

---
+++ src/main/java/chapter2/task2/BaseOfNumberInNumberSystem.java
@@ -0,0 +1,13 @@
+/**
+ * 6. В системе счисления с некоторым основанием десятичное число 81
+ * записывается в виде 100. Найти это основание.
+ */
+package chapter2.task2;
+
+public class BaseOfNumberInNumberSystem {
+
+    public static void main(String[] args) {
+
+
+    }
+}

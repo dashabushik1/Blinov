d8c875359c4fd51178356dbb961037f0858e9f8a
Darya Bushik <darya.bushik@gmail.com>
2022-09-02 18:30:13 +0300

    change task

--- src/main/java/chapter3/task1/student/Main.java
+++ src/main/java/chapter3/task1/student/Main.java
@@ -5,58 +5,75 @@ public class Main {
     public static void main(String[] args) {
 
         Student[] students = new Student[]{
-                new Student(1L, 2345612L, "Petrov Ivan", "Minsk, Kaganca street 6/170",
-                        "Faculty of Computer Design", 2, 122, 1995),
-                new Student(2L, 5674839L, "Baranov Ilya", "Minsk, Aranskaya street 78/30",
-                        "Faculty of Information Technology and Management", 3, 67, 1996),
-                new Student(3L, 653490L, "Strelkova Olga", "Minsk, Kiseleva street 34/189",
-                        "Faculty of Computer Design", 4, 115, 1994),
-                new Student(4L, 8906789L, "Jukov Evgeny", "Minsk, Asipenki street 56/290",
-                        "Faculty of Computer Systems and Networks", 2, 122, 1995),
-                new Student(5L, 7832619L, "Blinov Anatoly", "Minsk, Krapotkina street 3/330",
-                        "Faculty of Computer Systems and Networks", 3, 123, 1996),
-                new Student(6L, 8908880L, "Antonova Ekaterina", "Minsk, Cimiriazieva street 54/178",
-                        "Faculty of Computer Design", 4, 122, 1994),
-                new Student(7L, 4543627L, "Antoschenka Kristina", "Minsk, Adoeuskogo street, 67/88",
-                        "Faculty of Information Technology and Management", 3, 67, 1995),
-                new Student(8L, 6754030L, "Petrova Alina", "Minsk, Filimonova street 55/102",
-                        "Faculty of Computer Systems and Networks", 3, 123, 1993),
+                new Student(1L, 2567834L, "Petrov", "Ivan", "Ivanovich", "Minsk, Kaganca street 6/170",
+                        "Faculty of Computer Design", 12, 12, 1995, 3, 199),
+                new Student(2L, 3784531L, "Baranov", "Ilya", "Alekseevich", "Minsk, Aranskaya street 78/30",
+                        "Faculty of Information Technology and Management", 13, 8, 1994, 4, 196),
+                new Student(3L, 4899006L, "Strelkova", "Olga", "Ivanovna", "Minsk, Kiseleva street 34/189",
+                        "Faculty of Computer Design", 6, 3, 1996, 2, 195),
+                new Student(4L, 2904532L, "Jukov", "Evgeny", "Petrovich", "Minsk, Asipenki street 56/290",
+                        "Faculty of Computer Systems and Networks", 9, 9, 1995, 1, 199),
+                new Student(5L, 3901212L, "Blinov", "Anatoly", "Olegovich", "Minsk, Krapotkina street 3/330",
+                        "Faculty of Computer Systems and Networks", 15, 7, 1996, 3, 199),
+                new Student(6L, 4785640L, "Antonova", "Ekaterina", "Vladimirovna", "Minsk, Cimiriazieva street 54/178",
+                        "Faculty of Computer Design", 4, 8, 1997, 2, 196),
+                new Student(7L, 3678903L, "Antoschenka", "Kristina", "Pavlovna", "Minsk, Adoeuskogo street, 67/88",
+                        "Faculty of Information Technology and Management", 5, 5, 1994, 4, 196),
+                new Student(8L, 3906512L, "Petrova", "Alina", "Dmitrievna", "Minsk, Filimonova street 55/102",
+                        "Faculty of Computer Systems and Networks", 24, 4, 1995, 3, 195),
         };
 
-        Student[] studentsByOneFaculty = findByFaculty(students, "Faculty of Computer Systems and Networks");
+
+        Student[] studentsByFaculty = findStudentsByFaculty(students, "Faculty of Computer Systems and Networks");
         System.out.println("The students of the faculty of Computer Systems and Networks are: ");
-        for (int i = 0; i < studentsByOneFaculty.length; i++) {
-            System.out.println("Student: " + studentsByOneFaculty[i].getFullName());
+        for (int i = 0; i < studentsByFaculty.length; i++) {
+            System.out.println(studentsByFaculty[i].getSurname() + " " + studentsByFaculty[i].getName() + " " +
+                    studentsByFaculty[i].getPatronymic());
         }
 
+        System.out.println("");
+        System.out.println("Students by faculties and courses: ");
 
-        Student[] listOfStudentsBornAfterGivenYear = findStudentsAfterGivenYear(students, 1994);
-        System.out.println("All students born after 1994: ");
-        for (int i = 0; i < listOfStudentsBornAfterGivenYear.length; i++) {
-            if (listOfStudentsBornAfterGivenYear[i] != null) {
-                System.out.println("Student: " + listOfStudentsBornAfterGivenYear[i].getFullName());
+
+        Student[] studentsAfterYear = findStudentsAfterGivenYear(students, 1995);
+        System.out.println("");
+        System.out.println("The students born after 1995: ");
+        for (int i = 0; i < studentsAfterYear.length; i++) {
+            if (studentsAfterYear[i] != null) {
+                System.out.println(studentsAfterYear[i].getSurname() + " " + studentsAfterYear[i].getName() + " " +
+                        studentsAfterYear[i].getPatronymic());
+            }
+        }
+
+        Student[] studentsByGroup = findStudentsByGroup(students, 199);
+        System.out.println("");
+        System.out.println("The students of group 199: ");
+        for (int i = 0; i < studentsByGroup.length; i++) {
+            if (studentsByGroup[i] != null) {
+                System.out.println(studentsByGroup[i].getSurname() + " " + studentsByGroup[i].getName() + " " +
+                        studentsByGroup[i].getPatronymic());
             }
         }
     }
 
-    private static Student[] findByFaculty(Student[] students, String faculty) {
-        Student[] expectedFacultyStudents = new Student[students.length];
+    private static Student[] findStudentsByFaculty(Student[] students, String faculty) {
+        Student[] foundedStudents = new Student[students.length];
         int studentIndex = 0;
         for (Student student : students) {
             if (student.getFaculty().equals(faculty)) {
-                expectedFacultyStudents[studentIndex] = student;
+                foundedStudents[studentIndex] = student;
                 studentIndex++;
             }
         }
-        Student[] actualFacultyStudents = new Student[studentIndex];
+        Student[] actualFoundedStudents = new Student[studentIndex];
         for (int i = 0; i < studentIndex; i++) {
-            actualFacultyStudents[i] = expectedFacultyStudents[i];
+            actualFoundedStudents[i] = foundedStudents[i];
         }
-        return actualFacultyStudents;
+        return actualFoundedStudents;
     }
 
     private static Student[] findStudentsByFacultyAndCourse(Student[] students, String faculty, int course) {
-        Student[] filteredStudents = new Student[students.length]; // как вывести на консоль этот метод?
+        Student[] filteredStudents = new Student[students.length];
         for (int i = 0; i < students.length; i++) {
             if (students[i].getFaculty().equals(faculty) && students[i].getCourse() == course) {
                 filteredStudents[i] = students[i];
@@ -81,17 +98,27 @@ public class Main {
         return actualStudents;
     }
 
-    private static Student[] findStudentsAfterGivenYear(Student[] students, int yearOfBirth) {
-        Student[] studentsYearOfBirth = new Student[students.length];
-        int yearIndex = 0;
+    private static Student[] findStudentsAfterGivenYear(Student[] students, int date_of_birth_year) {
+        Student[] studentsAfterDefiniteYear = new Student[students.length];
+        int studentIndex = 0;
         for (Student student : students) {
-            if (student.getYearOfBirth() > yearOfBirth) {
-                studentsYearOfBirth[yearIndex] = student;
-                yearIndex++;
+            if (student.getDate_of_birth_year() > date_of_birth_year) {
+                studentsAfterDefiniteYear[studentIndex] = student;
+                studentIndex++;
             }
         }
-        return studentsYearOfBirth;
+        return studentsAfterDefiniteYear;
+    }
+
+    private static Student[] findStudentsByGroup(Student[] students, int group) {
+        Student[] studentsByGroup = new Student[students.length];
+        int studentIndex = 0;
+        for (Student student : students) {
+            if (student.getGroup() == group) {
+                studentsByGroup[studentIndex] = student;
+                studentIndex++;
+            }
+        }
+        return studentsByGroup;
     }
 }
-
-

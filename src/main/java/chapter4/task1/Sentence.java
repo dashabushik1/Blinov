6d38569a20b34737cb45bb42c838db1a533a4b2b
Darya Bushik <bushik.darya@gmail.com>
2022-10-14 20:30:39 +0300

    add comments and toString method

--- src/main/java/chapter4/task1/Sentence.java
+++ src/main/java/chapter4/task1/Sentence.java
@@ -1,18 +1,24 @@
 package chapter4.task1;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 class Sentence {
 
-    private List<Word> words = new ArrayList<>();
+    private List<Word> words = new ArrayList<>();  // Word - тип, который хранится в списке
 
-    public Sentence addWord(Word word) {
+    public Sentence addWord(Word word) { // метод добавляет слова в предложения
         words.add(word);
         return this;
     }
 
-    public List<Word> getWords() {
-        return words;
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Word word : words) {
+            sb.append(word);
+            sb.append(" ");
+        }
+        return sb.toString();
     }
 }
+

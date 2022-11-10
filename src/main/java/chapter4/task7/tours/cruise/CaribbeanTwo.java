9b3fef6f8e06182f8fc32b89948071dcabcb7093
Darya Bushik <bushik.darya@gmail.com>
2022-11-10 17:32:33 +0300

    change enum

--- src/main/java/chapter4/task7/tours/cruise/CaribbeanTwo.java
+++ src/main/java/chapter4/task7/tours/cruise/CaribbeanTwo.java
@@ -2,13 +2,13 @@ package chapter4.task7.tours.cruise;
 
 import chapter4.task7.tours.Tour;
 
-import static chapter4.task7.enums.Duration.TEN;
-import static chapter4.task7.enums.Food.FULL_BOARD;
+import static chapter4.task7.enums.Duration.*;
+import static chapter4.task7.enums.TypeOfService.FULL_BOARD;
 import static chapter4.task7.enums.Transfer.YES;
 
 public class CaribbeanTwo extends Tour {
     public CaribbeanTwo() {
-        super(TEN, FULL_BOARD, YES, 1000);
+        super(TEN_DAYS, FULL_BOARD, YES, 1000);
         this.type = "Cruise tours";
     }
 }

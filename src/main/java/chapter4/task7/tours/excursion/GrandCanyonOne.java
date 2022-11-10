1d7c85b899341d0426776814ccedd62a4dc4abf0
Darya Bushik <bushik.darya@gmail.com>
2022-11-10 17:35:48 +0300

    change enum

--- src/main/java/chapter4/task7/tours/excursion/GrandCanyonOne.java
+++ src/main/java/chapter4/task7/tours/excursion/GrandCanyonOne.java
@@ -2,13 +2,13 @@ package chapter4.task7.tours.excursion;
 
 import chapter4.task7.tours.Tour;
 
-import static chapter4.task7.enums.Duration.THREE;
-import static chapter4.task7.enums.Food.BREAKFAST;
+import static chapter4.task7.enums.Duration.THREE_DAYS;
+import static chapter4.task7.enums.TypeOfService.BREAKFAST;
 import static chapter4.task7.enums.Transfer.NO;
 
 public class GrandCanyonOne extends Tour {
     public GrandCanyonOne() {
-        super(THREE, BREAKFAST, NO, 300);
+        super(THREE_DAYS, BREAKFAST, NO, 300);
         this.type = "Excursion tours";
     }
 }
--- src/main/java/chapter4/task7/tours/excursion/GrandCanyonTwo.java
+++ src/main/java/chapter4/task7/tours/excursion/GrandCanyonTwo.java
@@ -2,13 +2,13 @@ package chapter4.task7.tours.excursion;
 
 import chapter4.task7.tours.Tour;
 
-import static chapter4.task7.enums.Duration.FIVE;
-import static chapter4.task7.enums.Food.BREAKFAST;
+import static chapter4.task7.enums.Duration.FIVE_DAYS;
+import static chapter4.task7.enums.TypeOfService.BREAKFAST;
 import static chapter4.task7.enums.Transfer.NO;
 
 public class GrandCanyonTwo extends Tour {
     public GrandCanyonTwo() {
-        super(FIVE, BREAKFAST, NO, 500);
+        super(FIVE_DAYS, BREAKFAST, NO, 500);
         this.type = "Excursion tours";
 
     }

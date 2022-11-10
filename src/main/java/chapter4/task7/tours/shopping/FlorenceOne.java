3bee2b0e6c19aa71aa5a756073a0c0cac7ec7789
Darya Bushik <bushik.darya@gmail.com>
2022-11-10 17:35:35 +0300

    change enum

--- src/main/java/chapter4/task7/tours/shopping/FlorenceOne.java
+++ src/main/java/chapter4/task7/tours/shopping/FlorenceOne.java
@@ -2,13 +2,13 @@ package chapter4.task7.tours.shopping;
 
 import chapter4.task7.tours.Tour;
 
-import static chapter4.task7.enums.Duration.ONE;
-import static chapter4.task7.enums.Food.ROOM_ONLY;
+import static chapter4.task7.enums.Duration.ONE_DAY;
+import static chapter4.task7.enums.TypeOfService.ROOM_ONLY;
 import static chapter4.task7.enums.Transfer.NO;
 
 public class FlorenceOne extends Tour {
     public FlorenceOne() {
-        super(ONE, ROOM_ONLY, NO, 200);
+        super(ONE_DAY, ROOM_ONLY, NO, 200);
         this.type = "Shopping tours";
     }
 }
--- src/main/java/chapter4/task7/tours/shopping/FlorenceTwo.java
+++ src/main/java/chapter4/task7/tours/shopping/FlorenceTwo.java
@@ -3,7 +3,7 @@ package chapter4.task7.tours.shopping;
 import chapter4.task7.tours.Tour;
 
 import static chapter4.task7.enums.Duration.TWO_HOURS;
-import static chapter4.task7.enums.Food.ROOM_ONLY;
+import static chapter4.task7.enums.TypeOfService.ROOM_ONLY;
 import static chapter4.task7.enums.Transfer.NO;
 
 public class FlorenceTwo extends Tour {

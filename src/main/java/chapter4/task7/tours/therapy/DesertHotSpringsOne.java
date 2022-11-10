58a28df48f3ad3fa486f131500ce980d8e6a07b7
Darya Bushik <bushik.darya@gmail.com>
2022-11-10 17:33:44 +0300

    change enum

--- src/main/java/chapter4/task7/tours/therapy/DesertHotSpringsOne.java
+++ src/main/java/chapter4/task7/tours/therapy/DesertHotSpringsOne.java
@@ -2,13 +2,13 @@ package chapter4.task7.tours.therapy;
 
 import chapter4.task7.tours.Tour;
 
-import static chapter4.task7.enums.Duration.FIVE;
-import static chapter4.task7.enums.Food.FULL_BOARD;
+import static chapter4.task7.enums.Duration.FIVE_DAYS;
+import static chapter4.task7.enums.TypeOfService.FULL_BOARD;
 import static chapter4.task7.enums.Transfer.NO;
 
 public class DesertHotSpringsOne extends Tour {
     public DesertHotSpringsOne() {
-        super(FIVE, FULL_BOARD, NO, 800);
+        super(FIVE_DAYS, FULL_BOARD, NO, 800);
         this.type = "Therapy tours";
     }
 }

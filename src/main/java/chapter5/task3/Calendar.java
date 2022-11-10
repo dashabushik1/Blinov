bf61552c492f4ade10718272d6e0c172bdac2f0e
Darya Bushik <bushik.darya@gmail.com>
2022-11-10 17:32:00 +0300

    change access modifier

--- src/main/java/chapter5/task3/Calendar.java
+++ src/main/java/chapter5/task3/Calendar.java
@@ -11,7 +11,7 @@ import java.util.Set;
 public class Calendar {
 
     private final Set<MonthDay> holidays = new HashSet<>();
-    HolidayData holidayData;
+    private HolidayData holidayData;
 
     public Calendar() {
         this.holidayData = new HolidayData();

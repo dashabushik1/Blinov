6bc563ba1035d293f16701f4b57d06d871c3ac4e
Darya Bushik <bushik.darya@gmail.com>
2022-11-26 16:54:31 +0300

    change data type of variable

--- src/main/java/chapter7/task3/User.java
+++ src/main/java/chapter7/task3/User.java
@@ -1,16 +1,16 @@
 package chapter7.task3;
 
 public class User {
-    private Long id;
+    private int id;
     private int age;
     private String name;
     private String country;
 
     public User(int age, String name, String country) {
-        this(null, age, name, country);
+        this(0, age, name, country);
     }
 
-    public User(Long id, int age, String name, String country) {
+    public User(int id, int age, String name, String country) {
         if (age == 0) {
             throw new RuntimeException("Age must not be positive!");
         }

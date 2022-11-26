7a40e0d7359930429ade19b049f591099e817960
Darya Bushik <bushik.darya@gmail.com>
2022-11-26 16:50:29 +0300

    change method

--- src/main/java/chapter7/task2/Main.java
+++ src/main/java/chapter7/task2/Main.java
@@ -4,37 +4,27 @@
 package chapter7.task2;
 
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner inputWord = new Scanner(System.in);
-
         // функция проверки двух слов на анаграммы с помощью функционального интерфейса Anagram
-        Anagram anagram = ((firstInputWord, secondInputWord) -> {
-            char[] firstWordChars = firstInputWord.toLowerCase().toCharArray();
-            char[] secondWordChars = secondInputWord.toLowerCase().toCharArray();
+        Anagram anagram = ((firstWord, secondWord) -> {
+            if (firstWord.length() != secondWord.length()) {
+                return false;
+            }
+
+            char[] firstWordChars = firstWord.toLowerCase().toCharArray();
+            char[] secondWordChars = secondWord.toLowerCase().toCharArray();
 
             Arrays.sort(firstWordChars);
             Arrays.sort(secondWordChars);
 
-            if (String.valueOf(firstWordChars)
-                    .trim()
-                    .contentEquals(String.valueOf(secondWordChars).trim())) {
-                System.out.println("Words '" + firstInputWord + "' and '" + secondInputWord + "' are anagrams.");
-            } else {
-                System.out.println("Words '" + firstInputWord + "' and '" + secondInputWord + "' are not anagrams.");
-            }
+            return Arrays.equals(firstWordChars, secondWordChars);
         });
 
-        System.out.println("Enter an input first line of strings");
-        String firstWord = inputWord.nextLine();
-        System.out.println("Enter an input second line of strings");
-        String secondWord = inputWord.nextLine();
-
-        anagram.checkForAnagram(firstWord, secondWord);
-        inputWord.close();
+        boolean result = anagram.checkForAnagram("thing", "night");
+        System.out.println(result);
     }
 }
 

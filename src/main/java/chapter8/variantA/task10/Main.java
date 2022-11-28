de97aebfa8e525c7ebf3b67224d5653b39e1ccdc
Darya Bushik <bushik.darya@gmail.com>
2022-11-28 18:14:41 +0300

    change code from charArray to charAt method

--- src/main/java/chapter8/variantA/task10/Main.java
+++ src/main/java/chapter8/variantA/task10/Main.java
@@ -10,15 +10,12 @@ public class Main {
                 "and junior school (from 8 to 11 years old). In some areas there are middle schools " +
                 "instead of junior schools, which take pupils from 9 to 12 years old. Primary schools have from 50-200 pupils.";
 
-        if ((text != null) && (text.length() > 0)) {
-            char[] charArray = text.toLowerCase().toCharArray();
-            int sum = 0;
-            for (int i = 0; i < charArray.length; i++) {
-                if (charArray[i] >= '0' && charArray[i] <= '9') {
-                    sum += Integer.parseInt(String.valueOf(charArray[i]));
-                }
+        int sum = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isDigit(text.charAt(i))) {
+                sum += Character.getNumericValue(text.charAt(i));
             }
-            System.out.println(sum);
         }
+        System.out.println(sum);
     }
 }

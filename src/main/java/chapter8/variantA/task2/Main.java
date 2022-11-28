add3351fed6b796df7f509cebf97c168e42543ac
Darya Bushik <bushik.darya@gmail.com>
2022-11-28 18:17:31 +0300

    delete if condition

--- src/main/java/chapter8/variantA/task2/Main.java
+++ src/main/java/chapter8/variantA/task2/Main.java
@@ -2,32 +2,24 @@
  * 2. В тексте каждую букву заменить ее порядковым номером в алфавите. При выводе в одной строке печатать текст
  * с двумя пробелами между буквами, в следующей строке внизу под каждой буквой печатать ее номер.
  */
-package chapter8.variantA.task2;
+package chapter8.variantA.task2;  // Значение ASCII для алфавитов нижнего регистра составляет от 97 до 122.
 
 public class Main {
 
     public static void main(String[] args) {
         String text = "Cinema is an integral part of the social life of people." +
                 "The movie combines not only stunts, special effects and scenery, but also the genuine art.";
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-        if ((text != null) && (text.length() > 0)) {
-            char[] textChar = text.toLowerCase().toCharArray();
-            for (char ch : textChar) {
-                if ((ch == '.') && (ch == ',')) {
-                    System.out.print("");
-                } else {
-                    System.out.print(ch + "  ");
-                }
-            }
-            for (char x : textChar) {
-                if (x == ' ') {
-                    System.out.print("");
-                } else {
-                    int i = alphabet.lastIndexOf(x); //метод возвращает индекс последнего вхождения символа в алфавите
-                    i += 1;
-                    System.out.print(i + "  ");
-                }
+        for (int i = 0; i < text.length(); i++) {
+            System.out.print(text.charAt(i) + " ");
+        }
+
+        System.out.println("");
+        for (int i = 0; i < text.length(); i++) {
+            char letter = text.toLowerCase().charAt(i);
+            if (Character.isLetter(letter)) {  // a = 97, z = 122
+                int position = (int) letter - 'a' + 1; // приведем символ(букву) к типу int и получим позицию в алфавите
+                System.out.print(position + " ");
             }
         }
     }

18d7534d43d34303787dda1261ff5aa201021ca6
Darya Bushik <bushik.darya@gmail.com>
2022-12-28 22:00:34 +0300

    change code

--- src/main/java/chapter10/variantC/task4/Main.java
+++ src/main/java/chapter10/variantC/task4/Main.java
@@ -5,39 +5,26 @@
 package chapter10.variantC.task4;
 
 import java.io.*;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-public class Main {   // todo
+public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File file = new File("/Users/daryabushik/Desktop/files/variantC/Student.txt");
         File changedFile = new File("/Users/daryabushik/Desktop/files/variantC/outputTask4.txt");
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
              BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(changedFile))) {
-            StringBuilder builder = new StringBuilder();
             String line;
-            String word;
-            int offset = 0;
-            Pattern pattern = Pattern.compile("\".{2,}\"");
             while ((line = bufferedReader.readLine()) != null) {
-                Scanner scanner = new Scanner(line);
-                while (scanner.hasNext()) {
-                    word = scanner.next();
-                    Matcher matcher = pattern.matcher(word);
-                    if (matcher.matches()) {
-                        builder.append(matcher.group().toUpperCase()).append(" ");
+                String[] words = line.split("[\\s]+");
+                for (String word : words) {
+                    if (word.length() > 2) {
+                        bufferedWriter.append(word.toUpperCase()).append(" ");
                     } else {
-                        builder.append(word).append(" ");
+                        bufferedWriter.append(word).append(" ");
                     }
                 }
-                bufferedWriter.write(builder.toString(), offset, (builder.length() - offset));
-                bufferedWriter.newLine();
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 }

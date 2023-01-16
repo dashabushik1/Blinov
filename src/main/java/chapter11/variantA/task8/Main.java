396d6e856a997e2c732017080e1706bf0d1682a3
Darya Bushik <bushik.darya@gmail.com>
2023-01-16 14:05:21 +0300

    changed ArrayList to List interface, deleted catch block

--- src/main/java/chapter11/variantA/task8/Main.java
+++ src/main/java/chapter11/variantA/task8/Main.java
@@ -9,23 +9,20 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         Path path = Paths.get("/Users/daryabushik/Desktop/collections/text.txt");
-        ArrayList<String> listOfLines = new ArrayList<>();
+        List<String> listOfLines = new ArrayList<>();
 
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(path.toFile()));
+        try (BufferedReader reader = new BufferedReader(new FileReader(path.toFile()))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 listOfLines.add(line);
             }
             Collections.sort(listOfLines);
-            reader.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
         System.out.println(listOfLines);
     }

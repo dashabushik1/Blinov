edce0f084c7ceaf4b309e3b9edc4b76f0dd7ee83
Darya Bushik <bushik.darya@gmail.com>
2023-01-16 13:42:17 +0300

    file changed paths, try-with-resources block, deleted catch block

--- src/main/java/chapter10/variantC/task3/Main.java
+++ src/main/java/chapter10/variantC/task3/Main.java
@@ -9,18 +9,16 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        File file = new File("/Users/daryabushik/Desktop/files/variantC/Student.txt");
-        File changedFile = new File("/Users/daryabushik/Desktop/files/variantC/outputTask3.txt");
+        File file = new File("./src/main/java/chapter10/variantC/task3/student.txt");
+        File changedFile = new File("./src/main/java/chapter10/variantC/task3/outputTask3.txt");
 
-        if (file.exists()) {
-            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-                 BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(changedFile))) {
-                String line;
-                while ((line = bufferedReader.readLine()) != null) {
-                    String reverse = new StringBuilder(line).reverse().toString();
-                    bufferedWriter.write(reverse);
-                    bufferedWriter.write(System.lineSeparator());
-                }
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(changedFile))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String reverse = new StringBuilder(line).reverse().toString();
+                bufferedWriter.write(reverse);
+                bufferedWriter.newLine();
             }
         }
     }

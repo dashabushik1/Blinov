34cb03d54c5d4502414ecc9d2b15e074cbcf3721
Darya Bushik <bushik.darya@gmail.com>
2023-01-16 13:43:03 +0300

    changed paths

--- src/main/java/chapter10/variantC/task4/Main.java
+++ src/main/java/chapter10/variantC/task4/Main.java
@@ -9,14 +9,15 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        File file = new File("/Users/daryabushik/Desktop/files/variantC/Student.txt");
-        File changedFile = new File("/Users/daryabushik/Desktop/files/variantC/outputTask4.txt");
+        File file = new File("./src/main/java/chapter10/variantC/task4/student.txt");
+        File changedFile = new File("./src/main/java/chapter10/variantC/task4/outputTask4.txt");
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
              BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(changedFile))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 String[] words = line.split("[\\s]+");
+                bufferedWriter.append(System.lineSeparator());
                 for (String word : words) {
                     if (word.length() > 2) {
                         bufferedWriter.append(word.toUpperCase()).append(" ");
